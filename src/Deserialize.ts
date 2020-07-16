8d02aeaa6e9a0712bb9c5fb15b14934534f56b0d
Mateusz Murza <mateusz.murza@netbulls.io>
2020-07-16 17:50:48 +0200

    Take care of null values

--- src/Deserialize.ts
+++ src/Deserialize.ts
@@ -10,24 +10,24 @@ function objectifyDocumentProperty(
     timestamp: typeof firebase.firestore.Timestamp,
 ): any {
     let modifiedItem: any = item;
+	if(item != null)
+		if(item.startsWith && typeof item === 'string') {
+			if(item.startsWith('__DocumentReference__')) {
+				const path = item.split('__DocumentReference__')[1];
+				modifiedItem = firestore.doc(path);
+			}
 
-    if(item.startsWith && typeof item === 'string') {
-        if(item.startsWith('__DocumentReference__')) {
-            const path = item.split('__DocumentReference__')[1];
-            modifiedItem = firestore.doc(path);
-        }
+			if(item.startsWith('__Timestamp__')) {
+				const dateString = item.split('__Timestamp__')[1];
+				modifiedItem = timestamp.fromDate(new Date(dateString));
+			}
 
-        if(item.startsWith('__Timestamp__')) {
-            const dateString = item.split('__Timestamp__')[1];
-            modifiedItem = timestamp.fromDate(new Date(dateString));
-        }
-
-        if(item.startsWith('__GeoPoint__')) {
-            const geoSection = item.split('__GeoPoint__')[1];
-            const [latitude, longitude] = geoSection.split('###');
-            modifiedItem = new geoPoint(parseFloat(latitude), parseFloat(longitude));
-        }
-    }
+			if(item.startsWith('__GeoPoint__')) {
+				const geoSection = item.split('__GeoPoint__')[1];
+				const [latitude, longitude] = geoSection.split('###');
+				modifiedItem = new geoPoint(parseFloat(latitude), parseFloat(longitude));
+			}
+		}
 
     return modifiedItem;
 }
